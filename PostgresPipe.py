096c0e6bdd007d91f18fc2e3320bc50900fa5a92
Andie Donovan <andiedonovan@yahoo.com>
2017-11-04 16:42:46 -0700

    Update PostgresPipe.py

--- PostgresPipe.py
+++ PostgresPipe.py
@@ -6,23 +6,14 @@ import ConfigParser
 
 Config = ConfigParser.ConfigParser()
 Config.read("config.ini")
-# Config.sections()  # see section csontents of config file
-# Config.options('SectionOne') # see arguments in first section
 
-Ydata = io.open(r'/Users/andiedonovan/myProjects/Youtube/Comments1.json', encoding = "utf-8")
+Ydata = io.open(r'/Users/Youtube/Comments1.json', encoding = "utf-8")
 readAll = Ydata.readlines()
-#json.dumps(readAll)
-
-#Df = pd.read_json('/Users/andiedonovan/myProjects/Youtube/Comments1.json')
-#Df.to_sql(connectionstring)
-
 
 conn = psycopg2.connect(Config.get('SectionOne', 'dbname', 'user', 'password')) # create a connection to databse; uses arguments provided in config.ini file
 cur = conn.cursor() # Create a cursor to execute SQL commands
 
 cur.execute("CREATE TABLE YTComments (data json);") #Create table called YTComments
-
-#cur.executemany("INSERT INTO YTComments VALUES ('{0}')".format(readAll))
 cur.execute("INSERT INTO YTComments VALUES (%s)" , readAll) # insert JSON formatted comments into table
 
 # side notes:
